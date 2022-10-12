0229260bb7c201b34e89d076ca6b508bcfa048e1
tingxuanhu <paperplant2022@gmail.com>
2022-10-12 18:30:01 +0800

    update

---
+++ src/wrapper/WrapperTransfer.java
@@ -0,0 +1,22 @@
+package wrapper;
+
+/**
+ * @author TingxuanHu
+ * @version 2022/10/12 10:17
+ */
+
+public class WrapperTransfer {
+    public static void main(String[] args) {
+        Integer i11 = 127;
+        int i12 = 127;
+
+        System.out.println(i11 == i12);
+
+
+
+
+    }
+
+
+
+}
